880bd00ecc74deb9881be055468a3d4db785ed75
lqwangxg <lqwangxg@gmail.com>
2020-09-23 07:47:58 +0000

    commit

---
+++ src/midware/sessionAuth.ts
@@ -0,0 +1,9 @@
+import session from "express-session";
+export const register = ( app: any ) => {
+    // Configure Express to use authentication sessions
+    app.use( session( {
+        resave: true,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET
+    } ) );
+}
