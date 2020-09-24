b13229201b519a7a093f67c0f1129aa64e2f74c6
lqwangxg <lqwangxg@gmail.com>
2020-09-24 03:59:13 +0000

    commit

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ if [ $? = 1 ]; then
  -v $app_name:/app \
  -v ~/npm/:/root/.npm/ \
  $docker_image_name \
- npm install \
- npm install --save-dev typescript @types/node@12 \
- npm install --save-dev ts-node nodemon rimraf npm-run-all \
- npm install --save-dev dev @types/dotenv \
- npm install --save-dev express-session @types/express-session \
- npm install --save-dev fs-extra @types/fs-extra \
- npm install --save-dev shelljs @types/shelljs
+ npm install 
+# npm install --save-dev typescript @types/node@12 \
+# npm install --save-dev ts-node nodemon rimraf npm-run-all \
+# npm install --save-dev dev @types/dotenv \
+# npm install --save-dev express-session @types/express-session \
+# npm install --save-dev fs-extra @types/fs-extra \
+#  npm install --save-dev shelljs @types/shelljs
 else
  echo "node_modules is exist, skip npm install. \n you can npm install packagename by ./edit.sh" 
 fi
