93586eaaf8f97610ccdc2e5b0b2fcffe25fc5109
lqwangxg <lqwangxg@gmail.com>
2020-09-24 06:51:14 +0000

    commit

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import passport from "passport"
 import bluebird from "bluebird"
 
 import { MONGODB_URI, SESSION_SECRET } from "./util/secrets"
-
+const MongoStore = mongo(session)
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home"
@@ -30,10 +30,29 @@ import * as passportConfig from "./config/passport"
 
 const app: express.Express = express()
 
+// connect to mongoDB
+//const mongoUrl = MONGODB_URI
+if (MONGODB_URI){
+  mongoose.Promise = bluebird
+  mongoose.connect(MONGODB_URI, 
+    { 
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true 
+    }
+  ).then( ()=>{
+   /* ready to use. */ 
+  }).catch(err =>{
+   console.error(`MongoDB connection error. Please make sure MongoDB is running. ${err}`)
+  })
+}
+
 app.set("port", process.env.SERVER_PORT || 3000)
 app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+//app.set("view engine", "ejs")
+app.set("view engine", "pug")
 
+app.use(compression())
 // CORS permission
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -44,38 +63,54 @@ app.use((req, res, next) => {
 // body-parser for request and response 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// simple logger
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.log('%s %s', req.method, req.url);
-  next();
-});
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.error(err);
-  next(err);
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: SESSION_SECRET,
+  store: new MongoStore({
+   url: MONGODB_URI,
+   autoReconnect: true
+  })
+}
+))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(lusca.xframe("SAMEORIGIN"))
+app.use(lusca.xssProtection(true))
+app.use((req, res, next) =>{
+  res.locals.user = req.user
+  next()
+})
+app.use((req, res, next) =>{
+  // After successful login, redirect back to the intended page
+  if(!req.user &&
+  req.path !== "/login" &&
+  req.path !== "/signup" &&
+  !req.path.match(/^\/auth/) &&
+  !req.path.match(/\./) {
+    req.session.returnTo = req.path
+  } else if (req.user && 
+  req.path == "/account"){
+    req.session.returnTo = req.path
+  }
+  next()
 })
 
 // for static files. like html, js, css, img and so on.
 app.use('/static', express.static(__dirname + '/public'));
 
-//sessionAuth.register(app)
 
-// GetとPostのルーティング
-const router: express.Router = express.Router()
-router.get('/', (req:express.Request, res:express.Response) => {
-  res.render("index")
-  //res.send("hello, this is the default page.")
-})
-router.get('/home', (req:express.Request, res:express.Response) => {
-  res.render("home")
-})
+/**
+ * Primary app routes 
+ */
+app.get("/", homeController.index)
+import userRouter from "./route/user.ts"
+import contactRouter from "./route/contact.ts"
+app.use("/user", userRouter)
+app.use("/contact",contactRouter)
 
-router.get('/api/getTest', (req:express.Request, res:express.Response) => {
-  res.json(req.query)
-})
-router.post('/api/postTest', (req:express.Request, res:express.Response) => {
-  res.json(req.body)
-})
-app.use(router)
+expert default app
 
 // 3000番ポートでAPIサーバ起動
-app.listen(port,()=>{ console.log(`webserver started at http://localhost:${port}`) })
+//app.listen(port,()=>{ console.log(`webserver started at http://localhost:${port}`) })
