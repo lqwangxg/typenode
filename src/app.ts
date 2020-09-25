3963818788203a60bea91a1b7f73120acae4eac3
lqwangxg <lqwangxg@gmail.com>
2020-09-25 01:59:40 +0000

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,78 +1,44 @@
+// Reqired Basic
 import express from 'express'
-import compression from "compression"
-import session from "express-session"
-import bodyParser from "body-parser"
-import lusca from "lusca"
-import mongo from "connect-mongo"
-import flash from "express-flash"
 import path from "path"
-import mongoose from "mongoose"
+
+// For security
+import compression from "compression"
+import lusca from "lusca"
+
+// For Auth
 import passport from "passport"
+
+// For session and error message
+import session from "express-session"
+import flash from "express-flash"
+
+// For mongoDB
+import mongo from "connect-mongo"
+import mongoose from "mongoose"
+
+// For Primese
 import bluebird from "bluebird"
+
+// For Logger
 import morgan from "morgan"
-
 import logger from "./util/logger"
-import { MONGODB_URI, SESSION_SECRET } from "./util/secrets"
-
-logger.debug("MONGODB_URI="+MONGODB_URI + ", SESSION_SECRET="+SESSION_SECRET)
-const MongoStore = mongo(session)
-
-// Controllers (route handlers)
-//import * as homeController from "./controllers/home"
-//import * as userController from "./controllers/user"
-//import * as apiController from "./controllers/api"
-//import * as contactController from "./controllers/contact"
-
-// API keys and passport configuration
-//import * as passportConfig from "./config/passport"
-//import passportlocal from "passport-local"
-//import passportFacebook from "passport-facebook"
-
 
 const app: express.Express = express()
 
-// connect to mongoDB
-if (MONGODB_URI){
-  mongoose.Promise = bluebird
-  mongoose.connect(MONGODB_URI, 
-    { 
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true 
-    }
-  ).then( ()=>{
-   /* ready to use. */ 
-  }).catch(err =>{
-   console.error(`MongoDB connection error. Please make sure MongoDB is running. ${err}`)
-  })
-
-  app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: "my-secret",
-    store: new MongoStore({
-      url: MONGODB_URI,
-      autoReconnect: true
-    })
-  }))
-}else{
-  app.use(session({ 
-    secret: "cookie-secret",
-    cookie: { maxAge: 60000 }
-  }))
-}
-
+// Global Parameters
 app.set("port", process.env.SERVER_PORT || 3000)
 app.set("mode", process.env.MODE || 'development')
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 //app.set("view engine", "pug")
 
-// logging: Standard Apache combined log output. 
+ 
 // options: combined,common,dev,short,tiny
 app.use(morgan("combined"))
 // attempt to compress response bodies for all request that traverse through
 app.use(compression())
+
 // CORS permission
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -87,43 +53,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+
 app.use(lusca.xframe("SAMEORIGIN"))
 app.use(lusca.xssProtection(true))
 
-/*
-app.use((req, res, next) =>{
-  res.locals.user = req.user
-  next()
-})
-*/
-/*
-app.use((req, res, next) =>{
-  // After successful login, redirect back to the intended page
-  if(!req.user &&
-  req.path !== "/login" &&
-  req.path !== "/signup" &&
-  !req.path.match(/^\/auth/) &&
-  !req.path.match(/\./) {
-    req.session.returnTo = req.path
-  } else if (req.user && 
-  req.path == "/account"){
-    req.session.returnTo = req.path
-  }
-  next()
-})
-*/
-
 // for static files. like html, js, css, img and so on.
 app.use('/static', express.static(__dirname + '/public'));
-
-
-/**
- * Primary app routes 
- */
-//app.get("/", homeController.index)
-//import userRouter from "./route/user.ts"
-//import contactRouter from "./route/contact.ts"
-//app.use("/user", userRouter)
-//app.use("/contact",contactRouter)
 
 export default app;
