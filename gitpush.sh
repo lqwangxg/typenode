0bb7f3a552203e3d4c9c77b8efafdf6810ca08b5
lqwangxg <lqwangxg@gmail.com>
2020-10-01 08:35:56 +0000

    commit

--- gitpush.sh
+++ gitpush.sh
@@ -3,4 +3,9 @@ git config --global user.name "lqwangxg"
 
 git add . 
 git commit -a -m "commit"
-git push -u origin master 
+
+branch=master
+if [ -n $1 ]; then # -z: is empty; -n: is not empty.
+  branch=$1
+fi
+git push -u origin $branch
