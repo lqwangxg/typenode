7443f7652a0d8db0036005d4926facf3d847b77e
lqwangxg <lqwangxg@gmail.com>
2020-10-05 08:08:32 +0000

    commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,19 @@
 import express from 'express'
+import userController from "../controllers/userController"
+import homeController from "../controllers/homeController"
 
 const router = express.Router();
+
+router.get("/user", userController.index)
+router.get("/home", homeController.index)
 
 router.get('/:userid', (req: express.Request, res: express.Response) =>{
     res.status(200).send({ message: 'welcome to access user pages,'+req.params["userid"] })
 })
 
-router.get('/', (req: express.Request, res: express.Response) =>{
-   res.status(200).send({ message: 'this is index of user' })
-})
+//router.get('/', (req: express.Request, res: express.Response) =>{
+//   res.status(200).send({ message: 'this is index of user' })
+//})
 
 //module.exports = router;
 export default router;
